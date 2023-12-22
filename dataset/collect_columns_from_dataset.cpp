5caecc672096b16a72e2ff507fcf61b4f55319a2
Kirill Tobola <kirilltobola@gmail.com>
2023-12-22 21:17:06 +0800

    replace tabs with 4 spaces

--- dataset/collect_columns_from_dataset.cpp
+++ dataset/collect_columns_from_dataset.cpp
@@ -18,47 +18,47 @@ const std::string SEP = "⇧";
 
 void create_files_list(std::vector<std::string> & file_names)
 {
-	/*
-	 * TODO: save result into file
-	 * Собирает в вектор имена файлов, которые надо посмотреть
-	 * Результат в векторе file_names. Последовательная ф-ция
-	 */
-	
-	std::string path = "dataset/Dataset/data";
+    /*
+     * TODO: save result into file
+     * Собирает в вектор имена файлов, которые надо посмотреть
+     * Результат в векторе file_names. Последовательная ф-ция
+     */
+    
+    std::string path = "dataset/Dataset/data";
 
-	std::vector<std::string> dir_names;
+    std::vector<std::string> dir_names;
     for (const auto & dir_name : std::filesystem::directory_iterator(path)) {
         // Check if it is a directory
-   		const std::filesystem::path path(dir_name);
-       	std::error_code ec;
-       	if (!std::filesystem::is_directory(path, ec)) {
-       		std::cout << "not a dir: " << dir_name << std::endl;
-       		continue;
-       	}
-       	
-		dir_names.push_back(dir_name.path().string());
-   	}
+           const std::filesystem::path path(dir_name);
+           std::error_code ec;
+           if (!std::filesystem::is_directory(path, ec)) {
+               std::cout << "not a dir: " << dir_name << std::endl;
+               continue;
+           }
+           
+        dir_names.push_back(dir_name.path().string());
+       }
 
-	// TODO: if file exists
-	std::ofstream out("filenames");
-   	for (auto dir_name : dir_names) {
-		for (const auto & file : std::filesystem::directory_iterator(dir_name)) {
-			std::string file_name = file.path().string();
+    // TODO: if file exists
+    std::ofstream out("filenames");
+       for (auto dir_name : dir_names) {
+        for (const auto & file : std::filesystem::directory_iterator(dir_name)) {
+            std::string file_name = file.path().string();
 
-			bool is_table_meta_info = std::regex_match(
-				file_name,
-				std::regex("(.)+table_(.)+_meta\.json")
-			);
+            bool is_table_meta_info = std::regex_match(
+                file_name,
+                std::regex("(.)+table_(.)+_meta\.json")
+            );
 
-			if (is_table_meta_info) {
-				file_names.push_back(file_name);
+            if (is_table_meta_info) {
+                file_names.push_back(file_name);
 
-				// save to file for optimization
-				out << file_name << std::endl;
-			}
-	   	}
-   	}
-   	out.close();
+                // save to file for optimization
+                out << file_name << std::endl;
+            }
+           }
+       }
+       out.close();
 }
 
 
@@ -66,86 +66,86 @@ void thread_function(const std::vector<std::string> & files_list)
 {
     int thread_id = omp_get_thread_num();
 
-	// TODO: rename me
-	std::vector<std::string> rows;
+    // TODO: rename me
+    std::vector<std::string> rows;
     
     #pragma omp for schedule(dynamic)
-	for (int i = 0; i < files_list.size(); i++) {
-		const std::string &file_name = files_list[i];
+    for (int i = 0; i < files_list.size(); i++) {
+        const std::string &file_name = files_list[i];
 
-		// Check if file is empty
-		if (std::filesystem::file_size(file_name) == 0) {
-			std::cout << "empty: " << file_name << std::endl;
-			continue;
-		}
+        // Check if file is empty
+        if (std::filesystem::file_size(file_name) == 0) {
+            std::cout << "empty: " << file_name << std::endl;
+            continue;
+        }
 
-		// Parse json file
-		FILE * fp = fopen(file_name.c_str(), "r");
-		char readBuffer[65536]; // TODO: decrease buffer size, its too big
-		rapidjson::FileReadStream is(
-			fp,
-			readBuffer,
-			sizeof(readBuffer)
-		);
-		rapidjson::Document d;
-		d.ParseStream(is);
-		fclose(fp);
+        // Parse json file
+        FILE * fp = fopen(file_name.c_str(), "r");
+        char readBuffer[65536]; // TODO: decrease buffer size, its too big
+        rapidjson::FileReadStream is(
+            fp,
+            readBuffer,
+            sizeof(readBuffer)
+        );
+        rapidjson::Document d;
+        d.ParseStream(is);
+        fclose(fp);
 
-		std::string table_id = file_name;
-		if (d["columns"].IsObject()) {
-			for (auto & column : d["columns"].GetObject()) {
-			
-				if (!column.name.IsString()) break;
-				std::string column_id = column.name.GetString();
+        std::string table_id = file_name;
+        if (d["columns"].IsObject()) {
+            for (auto & column : d["columns"].GetObject()) {
+            
+                if (!column.name.IsString()) break;
+                std::string column_id = column.name.GetString();
 
-				auto & propertyValue = column.value;
-				if (propertyValue.IsObject()) {
-					for (auto & column_properties : propertyValue.GetObject()) {
+                auto & propertyValue = column.value;
+                if (propertyValue.IsObject()) {
+                    for (auto & column_properties : propertyValue.GetObject()) {
 
-						if (!column_properties.name.IsString()) break;
-						std::string column_property_name = column_properties.name.GetString();
-						
-						if (column_property_name == "name") {
-							auto & column_property_value = column_properties.value;
+                        if (!column_properties.name.IsString()) break;
+                        std::string column_property_name = column_properties.name.GetString();
+                        
+                        if (column_property_name == "name") {
+                            auto & column_property_value = column_properties.value;
 
-							// Note: name value could be ARRAY!
-							if (column_property_value.IsString()) {
-								std::string column_name = column_property_value.GetString();
-								rows.push_back(table_id + SEP + column_id + SEP + column_name);
-								
-								// std::cout << "thread_id :" << thread_id << "works" << std::endl;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	std::cout << "thread_id: " << thread_id << " / done" << std::endl;
+                            // Note: name value could be ARRAY!
+                            if (column_property_value.IsString()) {
+                                std::string column_name = column_property_value.GetString();
+                                rows.push_back(table_id + SEP + column_id + SEP + column_name);
+                                
+                                // std::cout << "thread_id :" << thread_id << "works" << std::endl;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    std::cout << "thread_id: " << thread_id << " / done" << std::endl;
 
-	std::ofstream out("./columns/out_" + std::to_string(thread_id) + ".csv");
-	out << "table_id" + SEP + "column_id" + SEP + "column_name" << std::endl;
-	for (auto i : rows) {
-		out << i << std::endl;
-	}
-	out.close();
+    std::ofstream out("./columns/out_" + std::to_string(thread_id) + ".csv");
+    out << "table_id" + SEP + "column_id" + SEP + "column_name" << std::endl;
+    for (auto i : rows) {
+        out << i << std::endl;
+    }
+    out.close();
 }
 
 
 int main()
 {
-	std::vector<std::string> files_list;
-	if (std::filesystem::exists("filenames")) {
-		std::cout << "exists";
+    std::vector<std::string> files_list;
+    if (std::filesystem::exists("filenames")) {
+        std::cout << "exists";
 
-		std::ifstream in("filenames");
-		std::string file_name;
-		while (std::getline(in, file_name)) {
-			files_list.push_back(file_name);
-		}
-	} else {
-		create_files_list(files_list);
-	}
+        std::ifstream in("filenames");
+        std::string file_name;
+        while (std::getline(in, file_name)) {
+            files_list.push_back(file_name);
+        }
+    } else {
+        create_files_list(files_list);
+    }
 
 
     #pragma omp parallel 
@@ -153,7 +153,7 @@ int main()
         thread_function(files_list);
     }
 
-	std::cout << "done";
+    std::cout << "done";
 
-	return 0;
+    return 0;
 }
